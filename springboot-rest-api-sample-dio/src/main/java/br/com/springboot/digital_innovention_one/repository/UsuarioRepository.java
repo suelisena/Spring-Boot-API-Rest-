6eacce529da01a1c3ea0c87b6dc357eb2d0634cd
suelisena <suelissena@gmail.com>
2021-12-08 18:58:50 -0300

    commit para correção dos arquivos

---
+++ springboot-rest-api-sample-dio/src/main/java/br/com/springboot/digital_innovention_one/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package br.com.springboot.digital_innovention_one.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+import br.com.springboot.digital_innovention_one.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	List<Usuario> buscarPorNome(String name);
+
+}
+
